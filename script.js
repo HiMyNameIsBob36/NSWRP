0327e8f46ca620982f1589d3658df70f83c24959
HeyItsBob36 <166279266+HiMyNameIsBob36@users.noreply.github.com>
2026-01-13 23:09:45 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
  const images = [
     "assets/bg/01.png",
     "assets/bg/02.png",
-    "assets/bg/03.png"
-    "assets/bg/04.png"
+    "assets/bg/03.png",
+    "assets/bg/04.png",
     "assets/bg/05.png"
   ];
 
