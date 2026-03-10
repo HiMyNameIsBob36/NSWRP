7e85e6039ec2abee024ee5f8f35b95d3981a16f0
HeyItsBob36 <166279266+HiMyNameIsBob36@users.noreply.github.com>
2026-03-10 12:28:54 +1100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-/* --- DATA & STATS --- */
+/* --- DATA & STATS 
 const data = {
     "stats": {
         "uploads": "0",
@@ -17,7 +17,6 @@ function loadStats() {
     });
 }
 
-/* --- REVEAL ON SCROLL --- */
 function revealOnScroll() {
     const reveals = document.querySelectorAll(".reveal");
     const windowHeight = window.innerHeight;
@@ -27,7 +26,7 @@ function revealOnScroll() {
             el.classList.add("active");
         }
     });
-}
+}*/
 
 /* --- SEARCH LOGIC --- */
 const pages = [
@@ -61,35 +60,49 @@ function renderResults(query = "") {
 class SiteHeader extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-        <div id="overlay"></div>
-        <div class="navbar">
-            <button id="openNav" class="menuBtn button">☰</button>
-            <a href="https://aunsw.netlify.app/" class="logo"><img src="../media/logo.png"><span>New South Wales Roleplay</span></a>
-            <div class="nav-right"><img src="../media/search.png" id="searchBtn" class="button"></div>
-        </div>
-        <div id="sidebar" class="sidebar">
-            <div class="sidebarHeader">
-                <div class="serverName"><img src="../media/logo.png">New South Wales Roleplay</div>
-                <button id="closeNav" class="closeBtn button">✕</button>
-            </div>
-            <nav class="navList">
-                <details class="dropdown">
-                    <summary class="navItem"><img src="../media/departments.png"><span>Departments</span></summary>
-                    <div class="dropdownContent">
-                        <div class="dept-item"><img src="../media/department/police.png"><a href="/departments#police">Police</a></div>
-                        <div class="dept-item"><img src="../media/department/afp.png"><a href="/departments#afp">AFP</a></div>
-                        <div class="dept-item"><img src="../media/department/fire.png"><a href="/departments#fire">Fire</a></div>
-                    </div>
-                </details>
-                <a href="/meet-the-team" class="navItem"><span>Meet the Team</span></a>
-            </nav>
-        </div>
-        <div id="searchOverlay">
-            <div class="searchBox">
-                <input id="searchInput" type="text" placeholder="Search...">
-                <div id="searchResults"></div>
-            </div>
-        </div>`;
+    <div id="overlay"></div>
+<div class="navbar">
+
+<button id="openNav" class="menuBtn button">☰</button>
+
+<a href="https://aunsw.netlify.app/" class="logo"><img src="../media/logo.png"><span>New South Wales Roleplay</span></a>
+
+<div class="nav-right"><img src="../media/search.png" id="searchBtn" class="button"></div>
+</div>
+
+<div id="sidebar" class="sidebar"><div class="sidebarHeader">
+<div class="serverName"><img src="../media/logo.png">New South Wales Roleplay</div>
+
+<button id="closeNav" class="closeBtn button">✕</button></div>
+
+<nav class="navList">
+  <details class="dropdown">
+    <summary class="navItem"><img src="../media/departments.png"><span>Departments</span></summary>
+    <div class="dropdownContent">
+      <div class="dept-item"><img src="../media/department/police.png"><a href="https://aunsw.netlify.app/departments#police">Police</a></div>
+      <div class="dept-item"><img src="../media/department/afp.png"><a href="https://aunsw.netlify.app/departments#afp">Austrlaian Federal Police</a></div>
+      <div class="dept-item"><img src="../media/department/fire.png"><a href="https://aunsw.netlify.app/departments#fire">Fire & Rescue</a></div>
+      <div class="dept-item"><img src="../media/department/ambulance.png"><a href="https://aunsw.netlify.app/departments#ambulace">Paramedics</a></div>
+       <div class="dept-item"><img src="../media/department/dot.png"><a href="https://aunsw.netlify.app/departments#dot">Transport NSW</a></div>
+      </div></details>
+
+  <a href="https://aunsw.netlify.app/meet-the-team" class="navItem"><img src="../media/team.png"><span>Meet the Team</span></a>
+  <a href="https://aunsw.netlify.app/affiliates" class="navItem"><img src="../media/affiliates.png"><span>Affiliates</span></a>
+  <a href="https://aunsw.netlify.app/rules" class="navItem"><img src="../media/rulebook.png"><span>Rules & Guidelines</span></a>
+</nav>
+
+<div class="sidebarFooter">
+  <div class="copyright">© NSWRP | All rights reserved.</div>
+  <div class="social">
+    <a class="button socialBtn" href="https://www.youtube.com/@NSWERLC" target="_blank"><img src="../media/youtube.png"></a>
+    <a class="button socialBtn" href="https://discord.gg/rWtnWX5Esy" target="_blank"><img src="../media/discord.png"></a>
+  </div></div></div>
+
+<div id="searchOverlay">
+<div class="searchBox">
+<input id="searchInput" type="text" placeholder="Search...">
+<div id="searchResults"></div>
+</div></div>`;
 
         // Initialize listeners AFTER HTML is injected
         this.setupEventListeners();
